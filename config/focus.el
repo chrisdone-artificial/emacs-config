1b42068526aebdc4360b6cd23716c2ab29e4947a
Chris Done <chris.done@artificial.io>
2022-03-26 14:34:06 +0000

    Get paste working more reliably

--- config/focus.el
+++ config/focus.el
@@ -1,14 +1,19 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Focus state changes
+;;
+;; see accompanying scripts here: https://gist.github.com/chrisdone-artificial/89ff606c0e93dc2e39fb29b161b0bd41
 
-; (add-function :after after-focus-change-function 'my-after-focus)
+(add-function :after after-focus-change-function 'my-after-focus)
 (defun my-after-focus ()
-  ;; (when (frame-focus-state)
-  ;;   (send-string-to-terminal "\a"))
-  )
+  (when (frame-focus-state)
+    (send-string-to-terminal "\a")))
 
-(defun update-clipboard ()
+(defun update-clipboard (base64)
+  "Given a base64 string, decode it, strip final newline, and replace ^M with \n."
   (kill-new (with-temp-buffer
-              (insert-file-contents "~/.clipboard/clipboard.txt")
-              (buffer-substring (point-min) (1- (point-max)))))
-  nil)
+              (insert (base64-decode-string base64))
+              (replace-regexp-in-string
+               ""
+               "\n"
+               (buffer-substring (point-min) (1- (point-max))))))
+  "Clipboard updated.")

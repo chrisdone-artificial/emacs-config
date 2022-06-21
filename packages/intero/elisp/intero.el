cb4cf9f81df42cf5980e69061a53695d9197e91d
Chris Done <chris.done@artificial.io>
2022-06-21 15:57:22 +0100

    Change order of set fide

--- packages/intero/elisp/intero.el
+++ packages/intero/elisp/intero.el
@@ -2357,8 +2357,8 @@ Uses the default stack config file, or STACK-YAML file if given."
       (process-send-string process (format ":set -DSTACK_ROOT=%s\n" (intero-project-root)))
       (process-send-string process ":set prompt \"\\4\"\n")
       (process-send-string process ":set -fbyte-code -Wwarn\n")
+      (process-send-string process ":set -hiedir=.hie -fwrite-ide-info\n")
       (process-send-string process ":set -fdiagnostics-color=never\n")
-      (process-send-string process ":set -fwrite-ide-info -hiedir=.hie\n")
       buffer)))
 
 (defun intero-start-piped-process (buffer targets stack-yaml)
